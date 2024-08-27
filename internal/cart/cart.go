59d1661a2174ec898fa893b38da5c611a90db17e
Ethan Walker <ethan.walker830@gmail.com>
2024-08-26 17:40:26 -0700

    begin GB and GBC cart implementation

--- internal/cart/cart.go
+++ internal/cart/cart.go
@@ -7,13 +7,79 @@ import (
 
 type Cart interface{}
 
-type GBCart struct{}
+type GBCart struct {
+	Data   []byte
+	Header GBRomHeader
+}
 
-type GBACart struct{}
+type GBACart struct {
+	Data []byte
+}
 
-func LoadCart(cartPath string) (Cart, error) {
-	if _, err := os.Stat(cartPath); errors.Is(err, os.ErrNotExist) {
-		return nil, err
+type GBRomHeader struct {
+	EntryPoint           []byte
+	NintendoLogo         []byte
+	Title                []byte
+	ManufacturerCode     []byte
+	CGBFlag              byte
+	NewLicenseeCode      []byte
+	SGBFlag              byte
+	CartrigeType         byte
+	ROMSize              byte
+	RAMSize              byte
+	DestinationCode      byte
+	OldLicenseeCode      byte
+	MaskRomVersionNumber byte
+	HeaderChecksum       byte
+	GlobalChecksum       []byte
+}
+
+func NewGBRomHeader(data []byte) GBRomHeader {
+	header := GBRomHeader{
+		EntryPoint:           data[0x100:0x104],
+		NintendoLogo:         data[0x104:0x134],
+		Title:                data[0x134:0x144],
+		ManufacturerCode:     data[0x13F:0x143],
+		CGBFlag:              data[0x143],
+		NewLicenseeCode:      data[0x144:0x146],
+		SGBFlag:              data[0x146],
+		CartrigeType:         data[0x147],
+		ROMSize:              data[0x148],
+		RAMSize:              data[0x149],
+		DestinationCode:      data[0x14A],
+		OldLicenseeCode:      data[0x14B],
+		MaskRomVersionNumber: data[0x14C],
+		HeaderChecksum:       data[0x14D],
+		GlobalChecksum:       data[0x14E:0x150],
 	}
-	return GBCart{}, nil
+
+	return header
+}
+
+func (h *GBRomHeader) ComputeHeaderChecksum() {
+
+}
+
+func (h *GBRomHeader) ComputeGlobalChecksum() {
+
+}
+
+func LoadCart(cartPath string) (GBCart, error) {
+	fileInfo, err := os.Stat(cartPath)
+	if errors.Is(err, os.ErrNotExist) {
+		return GBCart{}, err
+	}
+
+	fp, err := os.Open(cartPath)
+	if err != nil {
+		return GBCart{}, err
+	}
+
+	data := make([]byte, fileInfo.Size())
+	_, err = fp.Read(data)
+	if err != nil {
+		return GBCart{}, err
+	}
+
+	return GBCart{Data: data, Header: NewGBRomHeader(data)}, nil
 }
---
+++ internal/cart/cart_test.go
@@ -0,0 +1,135 @@
+package cart_test
+
+import (
+	"testing"
+
+	"github.com/SocialSalt/goboy/internal/cart"
+	"github.com/google/go-cmp/cmp"
+)
+
+var nintendoLogo []byte = []byte{
+	0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E,
+}
+
+func TestGBCartReadPokemonCrystal(t *testing.T) {
+	cartPath := "/Users/ethanwalker/Library/Application Support/OpenEmu/Game Library/roms/Game Boy/Pokemon - Crystal Version (USA, Europe) (Rev 1).gbc"
+
+	loadedCart, err := cart.LoadCart(cartPath)
+
+	if err != nil {
+		t.Fatalf("Failed to load cart data %s", err)
+	}
+	if diff := cmp.Diff(nintendoLogo, loadedCart.Header.NintendoLogo); diff != "" {
+		t.Errorf("Failed to load nintendo logo. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff("PM_CRYSTAL\x00BYTE\xc0", string(loadedCart.Header.Title)); diff != "" {
+		t.Errorf("Failed to load the title correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff([]byte{0x42, 0x59, 0x54, 0x45}, loadedCart.Header.ManufacturerCode); diff != "" {
+		t.Errorf("Failed to load the manufacturer code correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0xC0), loadedCart.Header.CGBFlag); diff != "" {
+		t.Errorf("Failed to load the cgb flag correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff("01", string(loadedCart.Header.NewLicenseeCode)); diff != "" {
+		t.Errorf("Failed to load the new lisencsee code code correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x00), loadedCart.Header.SGBFlag); diff != "" {
+		t.Errorf("Failed to load the sgb flag correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x10), loadedCart.Header.CartrigeType); diff != "" {
+		t.Errorf("Failed to load the cartrige type correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x06), loadedCart.Header.ROMSize); diff != "" {
+		t.Errorf("Failed to load the rom size correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x03), loadedCart.Header.RAMSize); diff != "" {
+		t.Errorf("Failed to load the ram size correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x33), loadedCart.Header.OldLicenseeCode); diff != "" {
+		t.Errorf("Failed to load the old licensee code correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x01), loadedCart.Header.MaskRomVersionNumber); diff != "" {
+		t.Errorf("Failed to load the mask rom version number correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x26), loadedCart.Header.HeaderChecksum); diff != "" {
+		t.Errorf("Failed to load the header checksum correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff([]byte{0x18, 0xD2}, loadedCart.Header.GlobalChecksum); diff != "" {
+		t.Errorf("Failed to load the global checksum correctly. (-want +got) %s", diff)
+	}
+}
+
+func TestGBCartReadLinksAwakening(t *testing.T) {
+	cartPath := "/Users/ethanwalker/Library/Application Support/OpenEmu/Game Library/roms/Game Boy/Legend of Zelda, The - Link's Awakening (USA, Europe) (Rev 2).gb"
+
+	loadedCart, err := cart.LoadCart(cartPath)
+
+	if err != nil {
+		t.Fatalf("Failed to load cart data %s", err)
+	}
+	if diff := cmp.Diff(nintendoLogo, loadedCart.Header.NintendoLogo); diff != "" {
+		t.Errorf("Failed to load nintendo logo. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff("ZELDA\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00", string(loadedCart.Header.Title)); diff != "" {
+		t.Errorf("Failed to load the title correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff([]byte{0x00, 0x00, 0x00, 0x00}, loadedCart.Header.ManufacturerCode); diff != "" {
+		t.Errorf("Failed to load the manufacturer code correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x00), loadedCart.Header.CGBFlag); diff != "" {
+		t.Errorf("Failed to load the cgb flag correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff([]byte{0x0, 0x0}, loadedCart.Header.NewLicenseeCode); diff != "" {
+		t.Errorf("Failed to load the new lisencsee code code correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x00), loadedCart.Header.SGBFlag); diff != "" {
+		t.Errorf("Failed to load the sgb flag correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x03), loadedCart.Header.CartrigeType); diff != "" {
+		t.Errorf("Failed to load the cartrige type correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x04), loadedCart.Header.ROMSize); diff != "" {
+		t.Errorf("Failed to load the rom size correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x02), loadedCart.Header.RAMSize); diff != "" {
+		t.Errorf("Failed to load the ram size correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x01), loadedCart.Header.OldLicenseeCode); diff != "" {
+		t.Errorf("Failed to load the old licensee code correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x02), loadedCart.Header.MaskRomVersionNumber); diff != "" {
+		t.Errorf("Failed to load the mask rom version number correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff(byte(0x6A), loadedCart.Header.HeaderChecksum); diff != "" {
+		t.Errorf("Failed to load the header checksum correctly. (-want +got) %s", diff)
+	}
+
+	if diff := cmp.Diff([]byte{0x3A, 0xEE}, loadedCart.Header.GlobalChecksum); diff != "" {
+		t.Errorf("Failed to load the global checksum correctly. (-want +got) %s", diff)
+	}
+
+}
